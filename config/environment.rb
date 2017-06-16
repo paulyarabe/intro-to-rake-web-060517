4afb5fcc90b57a67e85fe3a1e93700b7faeab2ab
github username <github email address>
2017-06-16 14:46:50 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,8 @@
 require 'sqlite3'
 require 'pry'
 
+# where our Student Class is
 require_relative "../lib/student.rb"
 
+# the students database
 DB = {:conn => SQLite3::Database.new("db/students.db")}
